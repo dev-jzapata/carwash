7782324bad8f4274357ac3c86c32146edcf49d21
dev-jzapata <dev.jzapata@gmail.com>
2024-07-02 21:23:36 +0200

    servicio lavado creado, dependencias

---
+++ lavado-service/src/main/java/com/jzapata/lavado_service/LavadoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jzapata.lavado_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavadoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavadoServiceApplication.class, args);
+	}
+
+}
---
+++ lavado-service/src/test/java/com/jzapata/lavado_service/LavadoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzapata.lavado_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavadoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
