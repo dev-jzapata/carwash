d4a5165b7c883e23db9e5f7eab10a4bc12f3b389
dev-jzapata <dev.jzapata@gmail.com>
2024-07-02 18:18:23 +0200

    Proyecto modular creado

---
+++ src/test/java/com/jzapata/carwash/CarWashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzapata.carwash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
