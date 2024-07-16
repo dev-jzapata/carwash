fa2f2d742d7d0b10581d243126d7ec5e0323b218
dev-jzapata <dev.jzapata@gmail.com>
2024-07-16 17:27:34 +0200

    albaran creado, dependecias añadidas

---
+++ albaran-service/src/main/java/com/jzapata/albaran_service/AlbaranServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jzapata.albaran_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbaranServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbaranServiceApplication.class, args);
+	}
+
+}
---
+++ albaran-service/src/test/java/com/jzapata/albaran_service/AlbaranServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzapata.albaran_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbaranServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
